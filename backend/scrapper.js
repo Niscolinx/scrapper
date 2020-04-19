7f75338dbfeaf8157ea565adf6fce913658d3bee
Niscolinx <munisco12@gmail.com>
2020-04-19 22:20:47 +0100

    added axios config

--- backend/scrapper.js
+++ backend/scrapper.js
@@ -6,6 +6,10 @@ export async function getHtml(url){
     const { date: html } = await axios({
         method: 'get',
         url: url,
+        auth: {
+            username: 'munisco12@gmail.com',
+            password: 'munisco5'
+        },
     })
     .then(function(res) {
         console.log(res)
