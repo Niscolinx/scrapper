95e149b323840b0161345f0259484396167d5d51
Niscolinx <munisco12@gmail.com>
2020-04-19 21:48:20 +0100

    updated scrapper

--- backend/scrapper.js
+++ backend/scrapper.js
@@ -4,6 +4,12 @@ import cheerio from 'cheerio'
 export async function getHtml(url){
 
     const {date: html} = await axios.get(url)
+    .then(function(res) {
+        console.log(res)
+    })
+    .catch(function(err){
+        console.log(err)
+    })
     return html;
 }
 
