d600a26a92398ba1cac2fd9e911c57a1c9bb513b
Niscolinx <munisco12@gmail.com>
2020-04-20 14:39:36 +0100

    Changed the cli from anonymous function default async

--- backend/profile-scraper/cli.js
+++ backend/profile-scraper/cli.js
@@ -2,7 +2,7 @@
 
 const scraper = require('./scraper');
 
-(async () => {
+export default async function cli () => {
 	if(process.argv.length < 3 || !process.argv[2]) {
 		console.log('Usage : node backend/profile-scraper/cli.js LINKEDIN_URL');
 		return;
@@ -14,4 +14,4 @@ const scraper = require('./scraper');
 	} catch(e) {
 		console.error(e);
 	}
-})();
+};
