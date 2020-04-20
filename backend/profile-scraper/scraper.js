e67eaf4242723dbdddb7cf793cc0ab94bb5d8a72
Niscolinx <munisco12@gmail.com>
2020-04-20 12:16:16 +0100

    updated scrapper

--- backend/profile-scraper/scraper.js
+++ backend/profile-scraper/scraper.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const request = require('request-promise');
 const Entities = require('html-entities').XmlEntities;
  
-const config = require('../config');
+const config = require('./config');
 const debugFile = './assets/debug.json';
 const logger = require('@coya/logger')();
 
