b77910571eec0d345d4f63dcbf86100189b2081e
Niscolinx <munisco12@gmail.com>
2020-04-21 07:33:25 +0100

    updated cli

--- backend/profile-scraper/cli.js
+++ backend/profile-scraper/cli.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
-const express = require('express')
-
 const scraper = require('./scraper');
 
-const app = express()
 
 
-const cli = async () => {
+const cliasync () => {
 	if (process.argv.length < 3 || !process.argv[2]) {
 		console.log('Usage : node backend/profile-scraper/cli.js LINKEDIN_URL');
 	
@@ -21,14 +18,5 @@ const cli = async () => {
 };
 
 cli()
-
-app.get('/profile', (req, res, next) => {
-	console.log("starting to load the profiles"
-	)
-})
-
-app.listen(8000, () => {
-	console.log('Running on port http://localhost:8000')
-})
 
 
