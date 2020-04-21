228b76f27f93beabe2f7afbaf6a9fbf28a78381f
Niscolinx <munisco12@gmail.com>
2020-04-21 06:11:01 +0100

    Added my linkedIn url

--- backend/profile-scraper/config.js
+++ backend/profile-scraper/config.js
@@ -1,3 +1,6 @@
 module.exports = {
     cookie: 'AQEDASjJVlYBnjF2AAABcYJ8QtAAAAFxpojG0FYAmv27BQJTmd7Y8owqIbwsRBXugoAQvJi1GOa70RirtRjLkIaJ1Q7gJRUcRuC0S6ZwvGt2gOBRDCaopSE5X2ErTqxS28bLDyo-MlCYJ77vXGjXdAFV'
 };
+
+
+let linkedIn_profile = 'https://www.linkedin.com/in/igboanugwo-collins-4721b9171/'
