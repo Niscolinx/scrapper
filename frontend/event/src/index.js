b8aa348fa9035707badc2540c66676e9f03d87e2
Niscolinx <munisco12@gmail.com>
2020-04-22 12:52:09 +0100

    Added redux promise responder

--- frontend/event/src/index.js
+++ frontend/event/src/index.js
@@ -4,10 +4,32 @@ import count from './reducers/count'
 import aliases from '../aliases';
 
 const middleware = [
-    alias(aliases),  // this should always be the first middleware
-    // whatever middleware you want (like redux-thunk)
+    alias(aliases)
 ];
 
+/**
+ * Respond to action based on `redux-promise-middleware` result
+ * @param  {object} dispatchResult The resulting object from `store.dispatch()`
+ * @param  {func}   send           func to be called when sending response. Should be in form {value, error}
+ */
+const reduxPromiseResponder = (dispatchResult, send) => {
+    Promise
+        .resolve(dispatchResult.payload.promise) // pull out the promise
+        .then((res) => {
+            // if success then respond with value
+            send({
+                error: null,
+                value: res
+            });
+        })
+        .catch((err) => {
+            // if error then respond with error
+            send({
+                error: err,
+                value: null
+            });
+        });
+};
 
 
 import {wrapStore} from 'webext-redux';
