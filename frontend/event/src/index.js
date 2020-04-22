9e6cfa281dcad7d29ac7998397be4b40a31222aa
Niscolinx <munisco12@gmail.com>
2020-04-22 11:29:28 +0100

    moved the combineReducers to the index

--- frontend/event/src/index.js
+++ frontend/event/src/index.js
@@ -1,9 +1,12 @@
 import {createStore} from 'redux';
 import rootReducer from './reducers';
+import count from './reducers/count'
 
 import {wrapStore} from 'webext-redux';
 
-const store = createStore(rootReducer, {});
+const rootReducer = {
+    count
+}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(combineReducers(rootReducer), composeEnhancers(applyMiddleware(thunk)
