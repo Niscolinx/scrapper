71f0f49a6fb51432d380628e34ee2a987a46bd5b
Niscolinx <munisco12@gmail.com>
2020-04-22 12:42:06 +0100

    added aliases

--- frontend/event/src/index.js
+++ frontend/event/src/index.js
@@ -1,5 +1,5 @@
 import {createStore} from 'redux';
-import rootReducer from './reducers';
+import rootReducer from './reducers/aliases';
 import count from './reducers/count'
 import aliases from '../aliases';
 
