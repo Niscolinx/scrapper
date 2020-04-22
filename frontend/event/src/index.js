4f80a560ed2bd8cb1be4c7edaa42f38c98022a25
Niscolinx <munisco12@gmail.com>
2020-04-22 11:24:31 +0100

    integrated redux store

--- frontend/event/src/index.js
+++ frontend/event/src/index.js
@@ -5,6 +5,10 @@ import {wrapStore} from 'webext-redux';
 
 const store = createStore(rootReducer, {});
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(combineReducers(rootReducer), composeEnhancers(applyMiddleware(thunk)
+))
+
 wrapStore(store, {
     portName = "Munisco"
 });
