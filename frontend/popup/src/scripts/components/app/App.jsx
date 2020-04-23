97e80d839d4d8d1059c0403c09267155896256f8
Niscolinx <munisco12@gmail.com>
2020-04-23 08:33:48 +0100

    updated app

--- frontend/popup/src/scripts/components/app/App.jsx
+++ frontend/popup/src/scripts/components/app/App.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import * as actions from '../../../../../event/src/reducers/actions/burgerIndex'
+
 
 const getSession = () => {
   const data = {
