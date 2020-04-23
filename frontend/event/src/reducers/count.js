ff05a64fc137d639c2794c37110760231be16e13
Niscolinx <munisco12@gmail.com>
2020-04-23 07:49:11 +0100

    moved getsession() to the count component

--- frontend/event/src/reducers/count.js
+++ frontend/event/src/reducers/count.js
@@ -1,5 +1,16 @@
 const initialState = 0;
 
+const getSession = (originalAction) => {
+  return (dispatch, getState) => {
+    originalAction.payload = getState().session[originalAction._sender.tab.id];
+    return originalAction;
+  }
+};
+
+export default {
+  'GET_SESSION': getSession // the action to proxy and the new action to call
+};
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'GET_SESSION':
