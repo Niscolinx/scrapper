ad167b7d9a84af5e15cce2b2eca3d1b781fba4b1
Niscolinx <munisco12@gmail.com>
2020-04-23 08:42:44 +0100

    updated

--- frontend/event/src/reducers/actions/burgerIndex.js
+++ frontend/event/src/reducers/actions/burgerIndex.js
@@ -1,16 +1,4 @@
-export {
-    removeIngredient,
-    addIngredient,
-    setIngredients
-} from './burgerBuilder'
 
-export {
-    initBurgerStart,
-    orderRedirect,
-    fetchedOrdersInit,
-    fetchedOrderDelete,
-    deleteAllOrders
-} from './order'
 
 export {
     initAuth,
