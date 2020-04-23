6977b29e800d57dc6c4b20347910f77076908c62
Niscolinx <munisco12@gmail.com>
2020-04-23 20:28:38 +0100

    added fetch

--- frontend/popup/src/scripts/components/app/App.jsx
+++ frontend/popup/src/scripts/components/app/App.jsx
@@ -32,7 +32,7 @@ const mapStateToProps = state => {
 }
 const mapDispatchToProps = dispatch => {
   return {
-    onInitAuth: () => dispatch(actions.fetchGotten())
+    onInitAuth: () => dispatch(actions.fetch())
   }
 }
 
