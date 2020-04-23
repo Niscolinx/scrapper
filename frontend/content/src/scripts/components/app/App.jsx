2d3c830336c1d30936483e5aa89943fa5882c93b
Niscolinx <munisco12@gmail.com>
2020-04-23 20:50:36 +0100

    changed the code of the content script

--- frontend/content/src/scripts/components/app/App.jsx
+++ frontend/content/src/scripts/components/app/App.jsx
@@ -1,46 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../../../../../event/src/reducers/actions/burgerIndex'
 
-const getSession = () => {
-  const data = {
-    type: GET_SESSION,
-    payload: {} //payload is the data/object that is resolved by the promise
-  };
-  console.log(data)
-  return data;
-};
+
 class App extends Component {
   constructor(props) {
     super(props);
   }
 
   componentDidMount() {
-    // promise returned from `dispatch()`
-    this.props.dispatch(getSession())
-      .then((data) => {
-        console.log('this is the data from backend', data)
-        // the response data
-      })
-      .catch((err) => {
-        (console.log('this is the error from backend', err))
-        // something broke in the background store
-      });
+      this.props.onInitAuth()
   }
 
   render() {
     return (
       <div>
-        {this.props.session && this.props.users[this.props.session.userId].name}
+      {console.log('It is working wella')}
+        {this.props.loading}
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    session: state.session,
-    users: state.users
-  };
-};
 
-export default connect(mapStateToProps)(App);
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error,
+    auth: state.auth.tokenId,
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    onInitAuth: () => dispatch(actions.fetching())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
