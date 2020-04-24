4dcd8c6df966be1ad24bc6d8f42bafbc5f0106e9
Niscolinx <munisco12@gmail.com>
2020-04-24 17:23:17 +0100

    updated tweetFunc

--- frontend/event/src/reducers/actions/Auth.js
+++ frontend/event/src/reducers/actions/Auth.js
@@ -1,6 +1,6 @@
  import * as actions from './actionTypes'
  
-let tweetFunc = () => {
+export const tweetFunc = () => {
     getData()
         .then(info => checkIfExists(info))
         .then(info => {
