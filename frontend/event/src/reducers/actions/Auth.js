b11a29d299c33a4882a74c6fb7e34c50bce87a60
Niscolinx <munisco12@gmail.com>
2020-04-25 06:04:16 +0100

    updated Auth

--- frontend/event/src/reducers/actions/Auth.js
+++ frontend/event/src/reducers/actions/Auth.js
@@ -1,75 +1,75 @@
  import * as actions from './actionTypes'
  
-export const tweetFunc = () => {
-    getData(dispatch)
-        .then(info => checkIfExists(info))
-        .then(info => {
-            // create chrome notification
+// export const tweetFunc = () => {
+//     getData(dispatch)
+//         .then(info => checkIfExists(info))
+//         .then(info => {
+//             // create chrome notification
 
-            let options = {
-                type: "basic",
-                title: info.name,
-                message: info.desc,
-                iconUrl: "icon.png"
-            };
+//             let options = {
+//                 type: "basic",
+//                 title: info.name,
+//                 message: info.desc,
+//                 iconUrl: "icon.png"
+//             };
 
-            chrome.notifications.create(info.link, options);
+//             chrome.notifications.create(info.link, options);
 
-            return info;
-        })
-        .then(info => saveLink(info.link))
-        .catch(err => { console.log(err); callTweet(); });
+//             return info;
+//         })
+//         .then(info => saveLink(info.link))
+//         .catch(err => { console.log(err); callTweet(); });
 
 
-    function getData(dispatch) {
-        return new Promise((resolve, reject) => {
-            $.get('https://twitter.com/home', function (data) {
+//     function getData(dispatch) {
+//         return new Promise((resolve, reject) => {
+//             $.get('https://twitter.com/home', function (data) {
 
-                console.log('gotten to the data')
-                let tweet = $(data).find('div.tweet').eq(0);
-                let link = tweet.attr('data-permalink-path');
-                let name = tweet.find('strong.fullname').text();
-                let desc = tweet.find('p.tweet-text').text();
+//                 console.log('gotten to the data')
+//                 let tweet = $(data).find('div.tweet').eq(0);
+//                 let link = tweet.attr('data-permalink-path');
+//                 let name = tweet.find('strong.fullname').text();
+//                 let desc = tweet.find('p.tweet-text').text();
                 
-                resolve({ name, desc, link });
-                dispatch(callTweet());
-            });
-        });
-    }
+//                 resolve({ name, desc, link });
+//                 dispatch(callTweet());
+//             });
+//         });
+//     }
 
-    function checkIfExists(info) {
-        return new Promise((resolve, reject) => {
-            chrome.storage.local.get('links', function (data) {
-                var links = data.links || [];
-                if (links.indexOf(info.link) === -1) {
-                    // it's fine to show the notification
-                    resolve(info);
-                } else {
-                    reject('tweet already shown');
-                }
-            });
-        });
-    }
+//     function checkIfExists(info) {
+//         return new Promise((resolve, reject) => {
+//             chrome.storage.local.get('links', function (data) {
+//                 var links = data.links || [];
+//                 if (links.indexOf(info.link) === -1) {
+//                     // it's fine to show the notification
+//                     resolve(info);
+//                 } else {
+//                     reject('tweet already shown');
+//                 }
+//             });
+//         });
+//     }
 
-    function saveLink(link) {
-        return new Promise((resolve, reject) => {
-            chrome.storage.local.get('links', function (data) {
-                var links = data.links || [];
-                links.push(link);
-                chrome.storage.local.set({ links }, () => {
-                    resolve('done');
-                });
-            })
-        });
-    }
-};
+//     function saveLink(link) {
+//         return new Promise((resolve, reject) => {
+//             chrome.storage.local.get('links', function (data) {
+//                 var links = data.links || [];
+//                 links.push(link);
+//                 chrome.storage.local.set({ links }, () => {
+//                     resolve('done');
+//                 });
+//             })
+//         });
+//     }
+// };
 
-export const callTweet = () => {
-    setTimeout(tweetFunc, 2000);
-    return{
-        type: actions.AUTH_START
-    }
-};
+// export const callTweet = () => {
+//     setTimeout(tweetFunc, 2000);
+//     return{
+//         type: actions.AUTH_START
+//     }
+// };
 
 
 
