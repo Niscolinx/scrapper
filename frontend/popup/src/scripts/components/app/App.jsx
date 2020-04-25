7fd7ae1a1a7c93226d683dc998dc62d421888f6a
Niscolinx <munisco12@gmail.com>
2020-04-25 06:15:16 +0100

    trying to fetch when the pop loads

--- frontend/popup/src/scripts/components/app/App.jsx
+++ frontend/popup/src/scripts/components/app/App.jsx
@@ -1,45 +1,106 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import * as actions from '../../../../../event/src/reducers/actions/burgerIndex'
-
 
 let jsonData = []
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props)
+    }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      //jsonData.push(json)
-      .then(json => 
-        console.log(json),
+    componentDidMount() {
+        let tweetFunc = () => {
+            getData()
+                .then((info) => checkIfExists(info))
+                .then((info) => {
+                    // create chrome notification
+
+                    let options = {
+                        type: 'basic',
+                        title: info.name,
+                        message: info.desc,
+                        iconUrl: 'icon.png',
+                    }
+
+                    chrome.notifications.create(info.link, options)
+
+                    return info
+                })
+                .then((info) => saveLink(info.link))
+                .catch((err) => {
+                    console.log(err)
+                    callTweet()
+                })
+
+            function getData() {
+                return new Promise((resolve, reject) => {
+                    $.get('https://twitter.com/home', function (data) {
+                        console.log('gotten to the data')
+                        let tweet = $(data).find('div.tweet').eq(0)
+                        let link = tweet.attr('data-permalink-path')
+                        let name = tweet.find('strong.fullname').text()
+                        let desc = tweet.find('p.tweet-text').text()
+
+                        resolve({ name, desc, link })
+                    })
+                })
+            }
+
+            function checkIfExists(info) {
+                return new Promise((resolve, reject) => {
+                    chrome.storage.local.get('links', function (data) {
+                        var links = data.links || []
+                        if (links.indexOf(info.link) === -1) {
+                            // it's fine to show the notification
+                            resolve(info)
+                        } else {
+                            reject('tweet already shown')
+                        }
+                    })
+                })
+            }
+
+            function saveLink(link) {
+                return new Promise((resolve, reject) => {
+                    chrome.storage.local.get('links', function (data) {
+                        var links = data.links || []
+                        links.push(link)
+                        chrome.storage.local.set({ links }, () => {
+                            resolve('done')
+                            callTweet()
+                        })
+                    })
+                })
+            }
+        }
+
+        let callTweet = () => {
+            setTimeout(tweetFunc, 2000)
+        }
+        callTweet()
+    }
+
+    render() {
+        return (
+            <div>
+                {console.log('It is working wella')}
+                {jsonData}
+            </div>
         )
-  }
-
-  render() {
-    return (
-      <div>
-      {console.log('It is working wella')}
-        {jsonData}
-      </div>
-    );
-  }
+    }
 }
 
-
-const mapStateToProps = state => {
-  return {
-    loading: state.auth.loading,
-    error: state.auth.error,
-    auth: state.auth.tokenId,
-  }
+const mapStateToProps = (state) => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error,
+        auth: state.auth.tokenId,
+    }
 }
-const mapDispatchToProps = dispatch => {
-  return {
-   onInitAuth: () => dispatch(actions.tweetFunc())
-  }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onInitAuth: () => dispatch(actions.tweetFunc()),
+    }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
